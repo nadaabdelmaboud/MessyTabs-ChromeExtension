2d9b6d8ed865c8af492cb29a6c95d4797adf10df
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2020-06-23 00:23:37 +0200

    multiple links handle

--- background.js
+++
@@ -1,9 +0,0 @@
-console.log("this is background")
-chrome.browserAction.onClicked.addListener(ButtonClicked)
-
-function ButtonClicked(tab){
-let msg={
-    txt:"nada"
-}
-chrome.tabs.sendMessage(tab.id,msg);
-}
--- popup.js
+++ popup.js
@@ -7,18 +7,50 @@ function find(){
 let title=document.getElementById("title").value;
 chrome.tabs.query( {}, sendTitle)
 function sendTitle(tabs){
+    let retTabs=[];
+    if(title&&title!=""){
     for(var i=0;i<tabs.length;i++){
         let url=tabs[i].url.toString();
         let tabTitle=tabs[i].title.toString();
         if(tabTitle.includes(title)||url.includes(title)){
-       chrome.tabs.update(tabs[i].id, {selected: true});
-      chrome.windows.update(tabs[i].windowId,{focused: true} )
-      break;
+            retTabs.push(tabs[i]);
+
         }
 
     }
- 
+    if(retTabs.length==1){
+        chrome.tabs.update(retTabs[0].id, {selected: true});
+        chrome.windows.update(retTabs[0].windowId,{focused: true} )
+    }
+    else{
+        for(var i=0;i<retTabs.length;i++){
+            let div=document.getElementById("tabs");
+            var anchor = document.createElement('a');
+            anchor.setAttribute('href',retTabs[i].url);
+            anchor.innerText = retTabs[i].title;
+            anchor.style['display']="block";
+            anchor.addEventListener('click', getLink, false);
+            div.appendChild(anchor);
+        }
 
-}
+    }
+    }
    
+}
+}
+function getLink(){
+    let url=this.href;
+    chrome.tabs.query( {}, goTab)
+    function goTab(tabs){
+        for(var i=0;i<tabs.length;i++){
+            let urlTab=tabs[i].url.toString();
+            if(url==urlTab){
+               chrome.tabs.update(tabs[i].id, {selected: true});
+               chrome.windows.update(tabs[i].windowId,{focused: true} )
+               break;
+            }
+    
+        }
+    }
+    
 }
